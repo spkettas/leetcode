caa45775c699a97e0a44dd61c1cd506c5ccfd9e9
spkettas <spkettas@gmail.com>
2021-04-26 22:21:27 +0800

    feat: remove dupl link

--- src/link/L0201.java
+++ src/link/L0201.java
@@ -13,7 +13,7 @@ import java.util.Set;
  * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
  * <p>
  * 1.哈希表
- * 2.双重循环
+ * 2.暴力方式，双重循环
  */
 public class L0201 {
     // 1.哈希
@@ -29,7 +29,7 @@ public class L0201 {
             ListNode cur = pos.next; // 本身
 
             if (set.add(cur.val)) {
-                pos = pos.next;
+                pos = pos.next; // 前移
             } else {
                 pos.next = pos.next.next; // pos指针一直未移动，直接没有重复数
             }
@@ -39,11 +39,34 @@ public class L0201 {
         return head;
     }
 
+    // 2.暴力方式
+    public ListNode removeDuplicateNodes1(ListNode head) {
+        if (head == null) return null;
+
+        ListNode ob = head;
+        // &&& ob != null
+        while (ob != null) {
+            ListNode oc = ob;
+            while (oc.next != null) {
+                if (oc.next.val == ob.val) {
+                    oc.next = oc.next.next;
+                } else {
+                    oc = oc.next;
+                }
+            }
+
+            ob = ob.next;
+        }
+
+        return head;
+    }
+
     public static void main(String[] args) {
         var head = ListUtil.createLink(new int[]{1, 2, 3, 3, 3, 2, 1});
 
         L0201 l = new L0201();
-        var res = l.removeDuplicateNodes(head);
+        //var res = l.removeDuplicateNodes(head);
+        var res = l.removeDuplicateNodes1(head);
         MyPrint.showList(res);
     }
 }
